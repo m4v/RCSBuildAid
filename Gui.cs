133bc1f4df0d7e6a8cb9b438a25fd38daf821bc2
Elián Hanisch <lambdae2@gmail.com>
2013-09-06 09:56:45 -0300

    refactor Gui.Load a bit.

--- Gui.cs
+++ Gui.cs
@@ -59,40 +59,40 @@ namespace RCSBuildAid
 
         void Load ()
         {
-            int i;
-            bool b;
             settingsFile = Path.Combine (KSPUtil.ApplicationRootPath,
                                        "GameData/RCSBuildAid/settings.cfg");
             settings = ConfigNode.Load (settingsFile) ?? new ConfigNode ();
 
-            if (int.TryParse (settings.GetValue ("window_x"), out i)) {
-                winRect.x = i;
+            winRect.x = GetValue("window_x", winX);
+            winRect.y = GetValue("window_y", winY);
+            state = (WinState)GetValue("window_state", 0);
+            switchMode ();
+            DryCoM_Marker.other = GetValue("drycom_other", DryCoM_Marker.other);
+            DryCoM_Marker.fuel = GetValue("drycom_fuel", DryCoM_Marker.fuel);
+            DryCoM_Marker.oxidizer = DryCoM_Marker.fuel;
+            DryCoM_Marker.monopropellant = GetValue("drycom_mono", DryCoM_Marker.monopropellant);
+            CoMReference cref;
+            cref = (CoMReference)GetValue("com_reference", 0);
+            RCSBuildAid.SetReference(cref);
+            RCSBuildAid.rcsMode = (RCSMode)GetValue ("rcs_mode", 0);
+        }
+
+        int GetValue (string key, int defaultValue)
+        {
+            int value;
+            if (int.TryParse(settings.GetValue(key), out value)) {
+                return value;
             }
-            if (int.TryParse (settings.GetValue ("window_y"), out i)) {
-                winRect.y = i;
+            return defaultValue;
+        }
+
+        bool GetValue (string key, bool defaultValue)
+        {
+            bool value;
+            if (bool.TryParse(settings.GetValue(key), out value)) {
+                return value;
             }
-            if (int.TryParse (settings.GetValue ("window_state"), out i)) {
-                state = (WinState)i;
-                switchMode ();
-            } else {
-                state = WinState.none;
-            }
-            if (bool.TryParse (settings.GetValue ("drycom_other"), out b)) {
-                DryCoM_Marker.other = b;
-            }
-            if (bool.TryParse (settings.GetValue ("drycom_fuel"), out b)) {
-                DryCoM_Marker.fuel = b;
-                DryCoM_Marker.oxidizer = b;
-            }
-            if (bool.TryParse (settings.GetValue ("drycom_mono"), out b)) {
-                DryCoM_Marker.monopropellant = b;
-            }
-            if (int.TryParse (settings.GetValue ("com_reference"), out i)) {
-                RCSBuildAid.SetReference ((CoMReference)i);
-            }
-            if (int.TryParse (settings.GetValue ("rcs_mode"), out i)) {
-                RCSBuildAid.rcsMode = (RCSMode)i;
-            }
+            return defaultValue;
         }
 
         void Save ()

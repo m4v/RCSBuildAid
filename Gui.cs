ae5c16ca3fc8e875f3cdb02e1d8f5cbd91c0df1d
Elián Hanisch <lambdae2@gmail.com>
2013-12-28 07:36:55 -0300

    refactor how window is resized when switching menus

--- Gui.cs
+++ Gui.cs
@@ -33,14 +33,15 @@ namespace RCSBuildAid
         string title = "RCS Build Aid v0.4.1";
         int winX = 300, winY = 200;
         int winWidth = 178;
-        /* windows height for each WinState
-         * 26 + rows*25 */
-        int[] winHeight = { 51, 158, 114, 174 };
+        int minHeight = 51;
+        int maxHeight = 174;
+        WinState oldState;
+        int oldResourceCount;
 
         void Awake ()
         {
             winID = gameObject.GetInstanceID ();
-            winRect = new Rect (winX, winY, winWidth, winHeight[0]);
+            winRect = new Rect (winX, winY, winWidth, minHeight);
             Load ();
         }
 
@@ -63,7 +64,7 @@ namespace RCSBuildAid
 
             /* check if within screen */
             winRect.x = Mathf.Clamp (winRect.x, 0, Screen.width - winWidth);
-            winRect.y = Mathf.Clamp (winRect.y, 0, Screen.height - winHeight[(int)WinState.Mass]);
+            winRect.y = Mathf.Clamp (winRect.y, 0, Screen.height - maxHeight);
         }
 
         void Save ()
@@ -103,29 +104,33 @@ namespace RCSBuildAid
                 return;
             }
             /* Main button bar */
-            GUILayout.BeginHorizontal();
+            GUILayout.BeginHorizontal ();
             for (int i = 1; i < 4; i++) {
                 bool toggleState = (int)state == i;
-                if (GUILayout.Toggle(toggleState, ((WinState)i).ToString(), GUI.skin.button)) {
+                if (GUILayout.Toggle (toggleState, ((WinState)i).ToString (), GUI.skin.button)) {
                     if (!toggleState) {
                         /* toggling on */
                         state = (WinState)i;
-                        switchDisplayMode();
+                        switchDisplayMode ();
                     }
                 } else {
                     if (toggleState) {
                         /* toggling off */
                         state = WinState.none;
-                        switchDisplayMode();
+                        switchDisplayMode ();
                     }
                 }
             }
-            GUILayout.EndHorizontal();
+            GUILayout.EndHorizontal ();
 
             /* check display Mode changed and sync GUI state */
-            checkDisplayMode();
+            checkDisplayMode ();
 
-            winRect.height = winHeight[(int)state]; /* 'cause GUILayout doesn't shrink the window */
+            /* 'cause GUILayout doesn't shrink the window */
+            if (state != oldState) {
+                winRect.height = minHeight;
+                oldState = state;
+            }
             switch (state) {
             case WinState.RCS:
                 drawRCSMenu();
@@ -277,10 +282,10 @@ namespace RCSBuildAid
             GUILayout.BeginVertical ("Resources", GUI.skin.box);
             {
                 GUILayout.Space(GUI.skin.box.lineHeight + 4);
-                /* adjust window height */
-                int rows = DCoM_Marker.Resource.Count;
-                if (rows > 0) { 
-                    winRect.height += 15 * rows + 4 * (rows - 1);
+                /* reset window height */
+                if (DCoM_Marker.Resource.Count != oldResourceCount) {
+                    winRect.height = minHeight;
+                    oldResourceCount = DCoM_Marker.Resource.Count;
                 }
                 GUILayout.BeginHorizontal ();
                 {

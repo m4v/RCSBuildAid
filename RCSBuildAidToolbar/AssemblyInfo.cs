ab5a2d2f9f3f9915f19d8f34566f89c99ef06610
Elián Hanisch <lambdae2@gmail.com>
2014-01-06 17:29:10 -0300

    update copyright year, and add missing headers

--- RCSBuildAidToolbar/AssemblyInfo.cs
+++ RCSBuildAidToolbar/AssemblyInfo.cs
@@ -1,8 +1,24 @@
+/* Copyright © 2013-2014, Elián Hanisch <lambdae2@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("RCSBuildAidToolbar")]
 [assembly: AssemblyDescription("Aid tool for the placement of RCS thrusters.")]
 [assembly: AssemblyProduct("RCSBuildAid")]
-[assembly: AssemblyCopyright("Copyright © 2013, Elián Hanisch. Distributed under the terms of the LGPLv3")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014, Elián Hanisch. Distributed under the terms of the LGPLv3")]
+[assembly: AssemblyVersion("0.4.2.*")]

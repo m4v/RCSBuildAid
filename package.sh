15bc2a1177d02f62c56408b3101d1c5e99c27aff
Elián Hanisch <lambdae2@gmail.com>
2014-01-06 17:22:25 -0300

    update package script

--- package.sh
+++ package.sh
@@ -14,14 +14,21 @@ VERSION=${VERSION/.\*\")*/}
 rm -rf "$DIR"
 mkdir -vp "$DIR"
 mkdir -vp "$DIR/Plugins"
-mkdir -vp "$DIR/Sources"
+mkdir -vp "$DIR/Sources/${NAME}Toolbar"
+mkdir -vp "$DIR/Textures"
 
 cp -v "bin/Release/$NAME.dll" "$DIR/Plugins"
 cp -v *.cs "$DIR/Sources"
-cp -v *.txt "$DIR"
+cp -v Textures/*.png "$DIR/Textures"
+#cp -v *.txt "$DIR"
 cp -v *.asciidoc "$DIR"
+
+cp -v "${NAME}Toolbar/bin/Release/${NAME}Toolbar.dll" "$DIR/Plugins"
+cp -v RCSBuildAidToolbar/*.cs "$DIR/Sources/${NAME}Toolbar"
 
 cd Package
 ZIPNAME="${NAME}_v${VERSION}.zip"
 rm -f "$ZIPNAME"
 zip -r "$ZIPNAME" "$NAME"
+
+

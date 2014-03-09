5d5219c3879bc1b3836adb7542af22f473625df3
Elián Hanisch <lambdae2@gmail.com>
2014-03-09 14:29:47 -0300

    fix MOI calculation resulting zero when torque is zero.

--- MomentOfInertia.cs
+++ MomentOfInertia.cs
@@ -18,10 +18,13 @@ namespace RCSBuildAid
             /* Not sure if this moment of inertia matches the one vessels have in game */
             Vector3 distance = transform.position - (part.transform.position 
                 + part.transform.rotation * part.CoMOffset);
-            Vector3 distRotAxis = Vector3.Cross(distance, 
-                                                RCSBuildAid.ReferenceVector.Torque.normalized);
+            Vector3 axis = RCSBuildAid.ReferenceVector.Torque.normalized;
+            if (axis == Vector3.zero) {
+                axis = Vector3.up; /* there's no torque, any vector will do */
+            }
+            Vector3 distAxis = Vector3.Cross(distance, axis);
             float mass = part.mass + part.GetResourceMass();
-            value += mass * distRotAxis.sqrMagnitude;
+            value += mass * distAxis.sqrMagnitude;
 
             foreach (Part p in part.children) {
                 recursePart (p);

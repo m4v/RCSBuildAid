797565e62b91266ff51373a597355fecb86af4e2
Elián Hanisch <lambdae2@gmail.com>
2014-03-12 15:20:10 -0300

    fix exceptions when plugin enabled without parts.

--- RCSBuildAid.cs
+++ RCSBuildAid.cs
@@ -62,7 +62,7 @@ namespace RCSBuildAid
         public static Vector3 Normal {
             get {
                 if (referenceTransform == null) {
-                    throw new Exception ("No reference transform.");
+                    return Vector3.zero;
                 }
                 switch (direction) {
                 case Directions.forward:
@@ -340,6 +340,8 @@ namespace RCSBuildAid
             if (referenceTransform == null) {
                 if (EditorLogic.startPod != null) {
                     SetReferenceTransform(EditorLogic.startPod.GetReferenceTransform());
+                } else {
+                    return;
                 }
             }
 

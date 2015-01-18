m4v/RCSBuildAid

commit 6f18d119a353f07c71a19a05a736e361d0abbc87

fix again counting parts selected with gizmos twice.

Plugin/RCSBuildAid.cs
@@ -46,6 +46,8 @@ namespace RCSBuildAid
         public static GameObject ACoM;
 
         EditorVesselOverlays vesselOverlays;
+        List<PartModule> tempList;
+        Type partModuleType;
 
         /* Properties */
 
@@ -412,7 +414,7 @@ namespace RCSBuildAid
             }
         }
 
-        static void switchDirection (Directions dir)
+        void switchDirection (Directions dir)
         {
             Directions direction = events.direction;
             /* directions only make sense in RCS mode */
@@ -460,41 +462,55 @@ namespace RCSBuildAid
             WheelList.Clear ();
         }
 
-        static void recursePart<T> (Part part, List<PartModule> list) where T : PartModule
+        List<PartModule> getModulesOf<T> () where T : PartModule
+        {
+            tempList = new List<PartModule> ();
+            partModuleType = typeof(T);
+            runOnAllParts (findModules);
+            return tempList;
+        }
+
+        void findModules (Part part)
         {
             /* check if this part has a module of type T */
             foreach (PartModule mod in part.Modules) {
-                if (mod is T) {
-                    list.Add (mod);
+                if (mod.GetType() == partModuleType) {
+                    tempList.Add (mod);
                     break;
                 }
             }
-
-            foreach (Part p in part.children) {
-                recursePart<T> (p, list);
-            }
         }
-
-        public static List<PartModule> getModulesOf<T> () where T : PartModule
-        {
-            List<PartModule> list = new List<PartModule> ();
-
-            /* find modules connected to vessel */
-            if (EditorLogic.RootPart != null) {
-                recursePart<T> (EditorLogic.RootPart, list);
+            
+        public static void runOnAllParts(Action<Part> f) {
+            if (EditorLogic.RootPart == null) {
+                return;
             }
 
-            /* find selected module when they are about to be connected */
+            /* run in vessel's parts */
+            recursePart(EditorLogic.RootPart, f);
+
+            /* run in selected parts that are connected */
             if (EditorLogic.SelectedPart != null) {
                 Part part = EditorLogic.SelectedPart;
-                if (part.potentialParent != null) {
-                    recursePart<T> (part, list);
-                    foreach (Part p in part.symmetryCounterparts) {
-                        recursePart<T> (p, list);
+                if (!EditorLogic.fetch.ship.Contains (part) && (part.potentialParent != null)) {
+                    recursePart (part, f);
+
+                    List<Part>.Enumerator enm = part.symmetryCounterparts.GetEnumerator ();
+                    while (enm.MoveNext ()) {
+                        recursePart (enm.Current, f);
                     }
                 }
             }
-            return list;
         }
+
+        static void recursePart(Part part, Action<Part> f)
+        {
+            f (part);
+            List<Part>.Enumerator enm = part.children.GetEnumerator();
+            while (enm.MoveNext()) {
+                recursePart (enm.Current, f);
+            }
+        }
+
 	}
 }

Plugin/ComMarker.cs
@@ -82,34 +82,13 @@ namespace RCSBuildAid
             vectorSum = Vector3.zero;
             totalMass = 0f;
 
-            if (EditorLogic.RootPart == null) {
-                return Vector3.zero;
-            }
+            RCSBuildAid.runOnAllParts (calculateCoM);
 
-            recursePart (EditorLogic.RootPart);
-            if (EditorLogic.SelectedPart != null) {
-                Part part = EditorLogic.SelectedPart;
-                if (!EditorLogic.fetch.ship.Contains(part) && (part.potentialParent != null)) {
-                    recursePart (part);
-
-                    List<Part>.Enumerator enm = part.symmetryCounterparts.GetEnumerator();
-                    while (enm.MoveNext()) {
-                        recursePart (enm.Current);
-                    }
-                }
+            if (vectorSum.IsZero ()) {
+                return vectorSum;
             }
 
             return vectorSum / totalMass;
-        }
-
-        void recursePart (Part part)
-        {
-            calculateCoM(part);
-           
-            List<Part>.Enumerator enm = part.children.GetEnumerator();
-            while (enm.MoveNext()) {
-                recursePart (enm.Current);
-            }
         }
 
         protected abstract void calculateCoM (Part part);

Plugin/MomentOfInertia.cs
@@ -33,21 +33,11 @@ namespace RCSBuildAid
                 return;
             }
             value = 0f;
-            recursePart(EditorLogic.RootPart);
-            if (EditorLogic.SelectedPart != null) {
-                Part part = EditorLogic.SelectedPart;
-                if (part.potentialParent != null) {
-                    recursePart (part);
 
-                    List<Part>.Enumerator enm = part.symmetryCounterparts.GetEnumerator();
-                    while (enm.MoveNext()) {
-                        recursePart (enm.Current);
-                    }
-                }
-            }
+            RCSBuildAid.runOnAllParts (calculateMoI);
         }
 
-        void recursePart (Part part)
+        void calculateMoI (Part part)
         {
             if (part.hasPhysicsEnabled ()) {
                 /* Not sure if this moment of inertia matches the one vessels have in game */
@@ -55,10 +45,6 @@ namespace RCSBuildAid
                     + part.transform.rotation * part.CoMOffset);
                 Vector3 distAxis = Vector3.Cross (distance, axis);
                 value += part.GetTotalMass() * distAxis.sqrMagnitude;
-            }
-
-            foreach (Part p in part.children) {
-                recursePart (p);
             }
         }
     }

commit 3a98e68b763a2ff45641dbd3147e94f78cf7357c

6f18d119 broke detecting PartModules derived from stock PartModules.

Plugin/RCSBuildAid.cs
@@ -476,7 +476,7 @@ namespace RCSBuildAid
         {
             /* check if this part has a module of type T */
             foreach (PartModule mod in part.Modules) {
-                if (mod.GetType() == partModuleType) {
+                if ((mod.GetType() == partModuleType) || mod.GetType().IsSubclassOf(partModuleType)) {
                     tempList.Add (mod);
                     break;
                 }
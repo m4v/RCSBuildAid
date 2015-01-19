m4v/RCSBuildAid

commit 4e7589ab32784841231ac200fe151ac9584101ea

refactor some old code

Plugin/GUI/MainWindow.cs
@@ -32,7 +32,6 @@ namespace RCSBuildAid
         bool settings = false;
         bool shortcut_selection = false;
         string title = "RCS Build Aid v0.5.4";
-        int winX = 270, winY = 50;
         int minWidth = 184;
         int maxWidth = 184;
         int minHeight = 52;
@@ -62,7 +61,7 @@ namespace RCSBuildAid
         void Awake ()
         {
             winID = gameObject.GetInstanceID ();
-            winRect = new Rect (winX, winY, minWidth, minHeight);
+            winRect = new Rect (Settings.window_x, Settings.window_y, minWidth, minHeight);
             winCBodyListRect = new Rect ();
             Load ();
             onDrawModeContent = null;
@@ -73,8 +72,6 @@ namespace RCSBuildAid
             RCSBuildAid.events.onModeChange += gameObject.AddComponent<MenuTranslation> ().onModeChange;
             RCSBuildAid.events.onModeChange += gameObject.AddComponent<MenuEngines> ().onModeChange;
             RCSBuildAid.events.onModeChange += gameObject.AddComponent<MenuAttitude> ().onModeChange;
-
-            RCSBuildAid.events.onSave += Save;
 #if DEBUG
             onDrawToggleableContent += gameObject.AddComponent<MenuDebug> ().DrawContent;
 #endif
@@ -87,18 +84,11 @@ namespace RCSBuildAid
 
         void Load ()
         {
-            winRect.x = Settings.GetValue ("window_x", winX);
-            winRect.y = Settings.GetValue ("window_y", winY);
-
             /* check if within screen */
             winRect.x = Mathf.Clamp (winRect.x, 0, Screen.width - maxWidth);
             winRect.y = Mathf.Clamp (winRect.y, 0, Screen.height - maxHeight);
-        }
-
-        void Save ()
-        {
-            Settings.SetValue ("window_x", (int)winRect.x);
-            Settings.SetValue ("window_y", (int)winRect.y);
+            Settings.window_x = (int)winRect.x;
+            Settings.window_y = (int)winRect.y;
         }
 
         void OnGUI ()

Plugin/Settings.cs
@@ -58,6 +58,8 @@ namespace RCSBuildAid
         public static bool menu_minimized;
         public static bool applauncher;
         public static bool action_screen;
+        public static int window_x;
+        public static int window_y;
         public static Dictionary<string, bool> resource_cfg = new Dictionary<string, bool> ();
 
         public static void LoadConfig ()
@@ -85,6 +87,8 @@ namespace RCSBuildAid
             applauncher      = GetValue ("applauncher"     , true );
             action_screen    = GetValue ("action_screen"   , false);
             toolbar_plugin   = GetValue ("toolbar_plugin"  , true );
+            window_x         = GetValue ("window_x"        , 280  );
+            window_y         = GetValue ("window_y"        , 114  );
 
             /* for these resources, set some defaults */
             resource_cfg ["LiquidFuel"] = GetValue (resourceKey ("LiquidFuel"), false);
@@ -115,6 +119,8 @@ namespace RCSBuildAid
             SetValue ("applauncher"     , applauncher     );
             SetValue ("action_screen"   , action_screen   );
             SetValue ("toolbar_plugin"  , toolbar_plugin  );
+            SetValue ("window_x"        , window_x        );
+            SetValue ("window_y"        , window_y        );
 
             if (direction != Directions.none) {
                 SetValue ("direction", (int)direction);

commit 24d6f70a7aa2a8fe89329bbc1674cc4f22a70847

fix saving window position saving code that broke in 4e7589ab3278484

Plugin/GUI/MainWindow.cs
@@ -74,6 +74,7 @@ namespace RCSBuildAid
             RCSBuildAid.events.onModeChange += gameObject.AddComponent<MenuTranslation> ().onModeChange;
             RCSBuildAid.events.onModeChange += gameObject.AddComponent<MenuEngines> ().onModeChange;
             RCSBuildAid.events.onModeChange += gameObject.AddComponent<MenuAttitude> ().onModeChange;
+            RCSBuildAid.events.onSave += Save;
 #if DEBUG
             onDrawToggleableContent += gameObject.AddComponent<MenuDebug> ().DrawContent;
 #endif
@@ -89,6 +90,10 @@ namespace RCSBuildAid
             /* check if within screen */
             winRect.x = Mathf.Clamp (winRect.x, 0, Screen.width - maxWidth);
             winRect.y = Mathf.Clamp (winRect.y, 0, Screen.height - maxHeight);
+        }
+
+        void Save ()
+        {
             Settings.window_x = (int)winRect.x;
             Settings.window_y = (int)winRect.y;
         }
m4v/RCSBuildAid

commit ac2f73e8ede9c9647b0c3e6747f023b314d47d7b

Fixes
Instead of fighting CoM's being one frame behind, delay all forces
one frame. This is simpler and fixes a variety of issues:

* vectors twitching when dragging the vessel.
* Vector's gimbal being wrong.
* losing marker vectors after removing the root part.

Plugin/ModuleForces.cs
@@ -96,8 +96,6 @@ namespace RCSBuildAid
             for (int i = 0; i < n; i++) {
                 obj = new GameObject ("PartModule Vector object");
                 obj.layer = gameObject.layer;
-                obj.transform.parent = transform;
-                obj.transform.position = thrustTransforms [i].position;
                 vectors [i] = obj.AddComponent<VectorGraphic> ();
                 vectors [i].setColor(color);
             }
@@ -106,6 +104,15 @@ namespace RCSBuildAid
 
         protected virtual void Update ()
         {
+        }
+
+        protected virtual void LateUpdate ()
+        {
+            /* we update forces positions in LateUpdate instead of parenting them to the part
+             * for prevent CoM position to be out of sync */
+            for (int i = 0; i < thrustTransforms.Count; i++) {
+                vectors [i].transform.position = thrustTransforms [i].position;
+            }
         }
 
         public void Enable ()

Plugin/MarkerForces.cs
@@ -32,8 +32,6 @@ namespace RCSBuildAid
         GameObject marker;
         MassEditorMarker comm;
 
-        bool hold_calculations;
-
         public MomentOfInertia MoI;
 
         public GameObject Marker {
@@ -112,25 +110,10 @@ namespace RCSBuildAid
             torqueCircle = getGameObject ("Torque Circle Object").AddComponent<CircularVectorGraphic> ();
 
             MoI = gameObject.AddComponent<MomentOfInertia> ();
-
-            Events.RootPartPicked += RootPartPicked;
-            Events.RootPartDropped += RootPartDropped;
         }
 
         void OnDestroy ()
         {
-            Events.RootPartPicked -= RootPartPicked;
-            Events.RootPartDropped -= RootPartDropped;
-        }
-
-        void RootPartPicked ()
-        {
-            hold_calculations = true;
-        }
-
-        void RootPartDropped ()
-        {
-            hold_calculations = false;
         }
 
         void Start ()
@@ -198,10 +181,6 @@ namespace RCSBuildAid
                 return;
             }
             transform.position = Marker.transform.position;
-
-            if (hold_calculations) {
-                return;
-            }
 
             /* calculate torque, translation and display them */
             calcMarkerForces (Marker.transform, out translation, out torque);

commit 31edd6487dcf0dfaf71a7381a8511ed54938fcd7

fix symmetry mode, broken by ac2f73e8ede9c9

Plugin/ModuleForces.cs
@@ -22,7 +22,7 @@ namespace RCSBuildAid
 {
     public abstract class ModuleForces : MonoBehaviour
     {
-        public VectorGraphic[] vectors = new VectorGraphic[0];
+        public VectorGraphic[] vectors;
 
         protected Color color = Color.cyan;
 
@@ -47,6 +47,11 @@ namespace RCSBuildAid
             Events.PluginEnabled -= onPluginEnabled;
             Events.PartChanged -= onPartChanged;
             RCSBuildAid.events.ModeChanged -= onModeChanged;
+
+            /* remove vectors */
+            for (int i = 0; i < vectors.Length; i++) {
+                Destroy (vectors [i].gameObject);
+            }
         }
 
         void onLeavingEditor ()
@@ -87,10 +92,6 @@ namespace RCSBuildAid
         {
             /* thrusterTransforms aren't initialized while in Awake, so in Start instead */
             GameObject obj;
-            if (vectors.Length > 0) {
-                /* clonned by symmetry, do nothing */
-                return;
-            }
             int n = thrustTransforms.Count;
             vectors = new VectorGraphic[n];
             for (int i = 0; i < n; i++) {
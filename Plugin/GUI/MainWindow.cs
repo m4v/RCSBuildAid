4c610ef320f7275e5484f11fe699569daae477ae
Elián Hanisch <lambdae2@gmail.com>
2016-06-04 00:10:42 -0300

    version 0.8

--- Plugin/GUI/MainWindow.cs
+++ Plugin/GUI/MainWindow.cs
@@ -30,7 +30,7 @@ namespace RCSBuildAid
         bool modeSelect;
         bool softLock;
         bool settings;
-        const string title = "RCS Build Aid v0.8-alpha";
+        const string title = "RCS Build Aid v0.8";
 
         KeybindConfig pluginShortcut;
 
